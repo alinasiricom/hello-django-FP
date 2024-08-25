fd6d0bf4df7bb32e8e3f93e51090ff5258b3196d
alinasiricom <nasiri.ali.r@gmail.com>
2024-08-25 09:11:08 +0330

    aapp / contact us

--- aapp/models.py
+++ aapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255,null=True, blank=True)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:     # change in admin view and sql commands
+        ordering = ['-created_date']
+    
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.id)
+    
+
+class Newsletter(models.Model):
+    email = models.EmailField()
+
+    def __str__(self):
+        return '{} ({})'.format(self.email, self.id)
--- aapp/views.py
+++ aapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
+from django.contrib import messages
+from .forms import *
 
 def index_view(request):
     return render(request, 'aapp/index.html')
@@ -6,5 +9,16 @@ def index_view(request):
 def about_view(request):
     return render(request, 'aapp/about-us.html')
 
+
+@csrf_protect
 def contact_view(request):
-    return render(request, 'aapp/contact-us.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Ticket successfully')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error ticket')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'aapp/contact-us.html', context)
