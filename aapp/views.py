8ea93f035f0551278832c12662cac640e474bf2c
alinasiricom <nasiri.ali.r@gmail.com>
2024-08-25 09:23:13 +0330

    add Newsletter

--- aapp/views.py
+++ aapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .forms import *
 
 def index_view(request):
@@ -22,3 +23,15 @@ def contact_view(request):
     form = ContactForm()
     context = {'form': form}
     return render(request, 'aapp/contact-us.html', context)
+
+
+@csrf_protect
+def newsletter_view(request):
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Newsletter successfully')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error Newsletter')
+    return HttpResponseRedirect('/')
